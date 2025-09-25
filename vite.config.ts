6d658959a293b7a0c94e0d86778ad784529d6e5f
Serhii Holovaschuk <serhiiholovaschuk@Serhiis-MacBook-Pro.local>
2025-09-25 19:51:48 +0300

    email_

--- vite.config.ts
+++ vite.config.ts
@@ -7,4 +7,9 @@ export default defineConfig({
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
+  server: {
+    host: 'soberwins.localhost',
+    port: 5173,
+    open: 'http://soberwins.localhost:5173/',
+  },
 });
